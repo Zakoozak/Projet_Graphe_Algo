5fc78fcc70e7fd9da852b2c3870df93af74b924f
Guillaume <guillaume.frc68@gmail.com>
2023-03-17 15:50:11 +0100

    ajout de la classe Arrete

--- src/Arrete.java
+++ src/Arrete.java
@@ -1,2 +1,46 @@
 public class Arrete {
+
+    private Sommet debut;
+    private Sommet fin;
+    private int poids;
+
+    public Arrete(Sommet debut, Sommet fin, int poids) {
+        this.debut = debut;
+        this.fin = fin;
+        this.poids = poids;
+    }
+
+    public Arrete(Sommet debut, Sommet fin) {
+        this(debut,fin,0);
+    }
+    public Sommet getDebut() {
+        return debut;
+    }
+
+    public Sommet getFin() {
+        return fin;
+    }
+
+    public int getPoids() {
+        return poids;
+    }
+
+    public void setDebut(Sommet debut) {
+        this.debut = debut;
+    }
+
+    public void setFin(Sommet fin) {
+        this.fin = fin;
+    }
+
+    public void setPoids(int poids) {
+        this.poids = poids;
+    }
+
+    @Override
+    public String toString() {
+        return "debut = " + debut +
+                ", fin = " + fin +
+                ", poids = " + poids;
+    }
 }
