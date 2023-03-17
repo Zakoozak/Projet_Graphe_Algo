b1949c806b9b6648a36b7b0884af429c8fe90526
Realexx <realex688@gmail.com>
2023-03-17 16:07:31 +0100

    Sommet + main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        Sommet s1 = new Sommet("Paris", 1);
+        System.out.println(s1);
+    }
+
 }
--- src/Sommet.java
+++ src/Sommet.java
@@ -1,2 +1,43 @@
 public class Sommet {
+    private String nom;
+    private int indice;
+    private int rang;
+
+    public Sommet(String nom, int indice) {
+        this.nom = nom;
+        this.indice = indice;
+    }
+
+    public Sommet(int indice) {
+        this("", indice);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getIndice() {
+        return indice;
+    }
+
+    public void setIndice(int indice) {
+        this.indice = indice;
+    }
+
+    public int getRang() {
+        return rang;
+    }
+
+    public void setRang(int rang) {
+        this.rang = rang;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + nom + "," + indice + "," + rang + "]" ;
+    }
 }
